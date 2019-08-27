079334b90fb75f29d42fe46956eeff7f96c62085
srikanthgai <51958505+srikanthgai@users.noreply.github.com>
2019-08-27 13:40:03 +0530

    Test code pushed to repository.

---
+++ Area of Square/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main {
+	public static void main (String[] args) {
+		// Type your code here
+      Scanner in = new Scanner(System.in);
+      int s = in.nextInt();
+      int square = s * s;
+      System.out.println(square);
+      
+	}
+}
---
+++ Sum of two digit number/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main {
+	public static void main (String[] args) {
+		// Type your code here
+      Scanner in = new Scanner(System.in);
+      int num = in.nextInt();
+      int sum = (num / 10) + (num % 10);
+      System.out.println(sum);
+	}
+}
